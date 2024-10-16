87cd85a48b615b3434c14deb10010e964ab11878
Ilya Grishanov <grishanov@me.com>
2024-10-16 19:47:52 +0300

    Initial Commit

---
+++ RGB-testTests/RGB_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  RGB_testTests.swift
+//  RGB-testTests
+//
+//  Created by mac on 16.10.2024.
+//
+
+import Testing
+@testable import RGB_test
+
+struct RGB_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
